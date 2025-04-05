ea16f2c52263fdb1e1df8c5fda866c1579eb3686
Matheus Almeida <matheuscalmeidac@gmail.com>
2025-04-05 09:33:18 -0300

    VelocidadeMoto

---
+++ Motoo.java
@@ -0,0 +1,15 @@
+package VelecidadeMoto;
+
+public class Motoo {
+    public static void main(String[] args) {
+        String marca = "Honda";
+        String modelo = "Pop 2000";
+        int ano = 2018;
+
+        System.out.println("A marca da moto é: " + marca);
+        System.out.println("O modelo da moto é: " + modelo);
+        System.out.println("O ano de fabricação da moto é: " + ano);
+
+        System.out.println("A moto está a 40KM/h");
+    }
+}
