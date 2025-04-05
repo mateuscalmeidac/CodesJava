f893dc1b4d8709d5c541f2de6ef0851561e5625f
Matheus Almeida <matheuscalmeidac@gmail.com>
2025-04-05 09:34:21 -0300

    CalculadoraJava

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
-package Aula2903Interface;
+package sobrecarga;
 
 public class Main {
-    public static void main(String[] args) {
-        // Criar instâncias de Produto (ou diretamente Eletronico)
-        Produto eletronico = new Eletronico("Smartphone", "Samsung", 2500.00);
+        public static void main(String[] args) {
+            int arroz = 2;
+            int feijão = 3;
 
-        // Exibir detalhes do produto
-        eletronico.exibirDetalhes();
+            double carne = 15.50;
+            double macarrao = 7.29;
+
+            Integer batata = 6;
+            Integer maracuja = 8;
+            Integer beterraba = 9;
+
+            int totalint = Calculadora.somar(arroz, feijão);
+            double totalDouble = Calculadora.somar(carne,macarrao);
+            Integer totalInteger = Calculadora.somar(batata, maracuja, beterraba);
+
+
+            System.out.println("O total de carne + macarrao é: " + totalDouble);
+            System.out.println("O total de batata, maracuja e beterraba é: " + totalInteger);
+
+        }
+
     }
-}
