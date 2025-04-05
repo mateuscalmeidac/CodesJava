c34e948780170ab1c7f49f2f176ba13817c75ccc
Matheus Almeida <matheuscalmeidac@gmail.com>
2025-04-05 09:35:06 -0300

    Testes

--- Main.java
+++ Main.java
@@ -1,25 +1,13 @@
-package sobrecarga;
+package Testes;
 
 public class Main {
         public static void main(String[] args) {
-            int arroz = 2;
-            int feijão = 3;
+            Pessoa pessoa = new Pessoa();
+            pessoa.setNome("João");
+            pessoa.setIdade(30);
 
-            double carne = 15.50;
-            double macarrao = 7.29;
-
-            Integer batata = 6;
-            Integer maracuja = 8;
-            Integer beterraba = 9;
-
-            int totalint = Calculadora.somar(arroz, feijão);
-            double totalDouble = Calculadora.somar(carne,macarrao);
-            Integer totalInteger = Calculadora.somar(batata, maracuja, beterraba);
-
-
-            System.out.println("O total de carne + macarrao é: " + totalDouble);
-            System.out.println("O total de batata, maracuja e beterraba é: " + totalInteger);
-
+            System.out.println("Nome: " + pessoa.getNome());
+            System.out.println("Idade: " + pessoa.getIdade());
         }
-
     }
+
---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+package Testes;
+
+public class Pessoa {
+        private String nome;
+        private int idade;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+        public void setIdade(int idade) {
+            if (idade >= 0) {
+// Validação
+                this.idade = idade;
+            } else {
+                System.out.println("Idade inválida!");
+            }
+        }
+    }
+
+
